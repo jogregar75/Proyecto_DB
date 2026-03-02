41ed291d98a0e8b4a1bcfe7f6797672ff676233d
José García <jogregar22@gmail.com>
2026-03-02 16:50:58 -0300

    v1

--- vite.config.ts
+++ vite.config.ts
@@ -19,3 +19,33 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+// import { componentTagger } from "lovable-tagger";
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   base: "/Proyecto_DB/", // 👈 IMPORTANTE para GitHub Pages
+
+//   server: {
+//     host: "::",
+//     port: 8080,
+//     hmr: {
+//       overlay: false,
+//     },
+//   },
+
+//   plugins: [
+//     react(),
+//     mode === "development" && componentTagger()
+//   ].filter(Boolean),
+
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
